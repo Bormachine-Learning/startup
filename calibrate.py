011420744fa38f09a91c5bfcd6f8402749dc0018
Bogdan Grigoruta <bogdangrigoruta@gmail.com>
2020-02-19 10:20:04 +0200

    update calibration script

--- calibrate.py
+++ calibrate.py
@@ -5,28 +5,28 @@ import glob
 # 25mm per square
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 25, 0.001)
 
-objp = np.zeros((6*7, 3), np.float32)
-objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+objp = np.zeros((6*9, 3), np.float32)
+objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 
 objpoints = []
 imgpoints = []
 
-images = glob.glob('calib_pics/*.jpeg')
+images = glob.glob('calib/*.jpeg')
 
 for image_file in images:
+    print(image_file)
     img = cv.imread(image_file)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
     # find corners
-    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
-
+    ret, corners = cv.findChessboardCorners(gray, (9, 6), None)
+    print(ret)
     if ret:
         objpoints.append(objp)
         # find more accurate corners
         acc_corners = cv.cornerSubPix(gray, corners, (11, 11), (-1, 1), criteria)
         imgpoints.append(acc_corners)
 
-        img = cv.drawChessboardCorners(img, (7, 6), acc_corners, ret)
+        img = cv.drawChessboardCorners(img, (9, 6), acc_corners, ret)
         cv.imshow("calibration image", img)
         cv.waitKey(500)
 
@@ -36,7 +36,7 @@ ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.sha
 
 # test undistortion on one image
 
-img = cv.imread("test15.jpeg")
+img = cv.imread("calib/test15.jpeg")
 height, width = img.shape[:2]
 new_matrix, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
 
@@ -45,12 +45,13 @@ x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
 cv.imwrite("calibresult.jpeg", dst)
 
+total_error = 0
 mean_err = 0
 for i in range(len(objpoints)):
     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    err = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
-    total_err += error
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
+    total_error += error
 
-mean_err = mean_err / len(objpoints)
+mean_err = total_error / len(objpoints)
 print(mean_err)
 
